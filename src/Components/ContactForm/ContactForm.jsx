46128673078f08dde9d9bb97aeedcdee563232ef
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-09-22 06:55:38 -0700

    I implemented the design and media querry of my about us page

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -15,19 +15,23 @@ import Brown from '../../asset/brown-design.png'
 const ContactForm = () => {
   return (
     <div>
-     <section className="section2-container">
-  <div class="section2">
-   <p class="g">Get In Touch</p>
-   {/* <article className="first"> */}
-    <p className='leave'>
-     Leave us a message
-    </p>
-   {/* </article> */}
-  </div>
-   <section className="sub-section2">
-     <form action="" className="form">
+      <section className="section1-container">
+        <h3 className='g'>Get In Touch</h3>
+        
+        
+        <section className="section1">
+        <p className='leave'>Leave us a message</p>
+        
+        
+        <section className="sub-section1">
+
+    
+    <div className="form">
+
+      <form>
        {/* First Name */}
       <label for="First_Name" class="first-name-label" id="first-name-label">Name</label>
+
       <input className="first-name" type="text" id="firstName" placeholder="First_Name_Last_Name"/>
       
         {/* Email */} 
@@ -37,10 +41,12 @@ const ContactForm = () => {
       <input className="text-here" type="text" placeholder="Your Message" id="textHere"/>
       
       {/* Submit button */}
-      <input className="submit" id="submit" type="submit" value="Send"/>
+       <input className="submit" id="submit" type="submit" value="Send"/>
      </form>
+      </div>
 
-    <article className="second">
+
+     <article className="second">
      <div className="gg">
       <div className="icon">
        <img src={Locate} alt="Location"/>
@@ -69,25 +75,40 @@ const ContactForm = () => {
      </div>
 
      <div className="socialss">
+      <div>
      <img src={Youtube} alt="Youtube"/>
+
+      </div>
+      <div>
+
      <img src={Instagram} alt="Instagram"/>
+      </div>
+      <div>
      <img src={Facebook} alt="Facebook"/>
+
+      </div>
+      <div>
      <img src={Twitter} alt="Twitter"/>
+
+      </div>
      </div>
      
      <img src={Location} className='Location' alt="Location"/>
   
 
+</article>
+
+
+        </section>
+
+        </section>
    <img className="dots2" src={Dot}alt="dots2"/>
 
    <img className="brown1" src={Brown} alt="brown-design"/>
   
    <img className="blue1" src={Blue} alt="blue-design"/>
-</article>
-
- </section>
-
- </section>
+      </section>
+  
 
     </div>
   )
--- src/Pages/AboutUsPage/AboutUs.jsx
+++ src/Pages/AboutUsPage/AboutUs.jsx
@@ -6,7 +6,7 @@ import Footer from '../../Components/Footer/footer'
 
 const AboutUs = () => {
   return (
-    <div>
+    <div className='about'>
       <section className="hero-circles">
         <div className="header">
           <Header/>
